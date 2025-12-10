839ce243d36c3bc702126f2f1168ac839173436a
teiniker <egon.teiniker@gmail.com>
2025-12-10 17:30:13 +0100

    Initial import.

---
+++ src/book_service/book.py
@@ -0,0 +1,7 @@
+
+class BookDTO:
+    """Transfer object for Book data"""
+    def __init__(self, oid:int, author:str, isbn:str) -> None:
+        self.oid = oid
+        self.author = author
+        self.isbn = isbn
